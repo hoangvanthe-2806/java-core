d887813d55a80e7a6e116301546269e5b20299c2
hoangthe030628 <hoangthe2862003@gmail.com>
2025-11-13 15:36:38 +0700

    on tap if-else

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,10 +5,20 @@ import java.util.Scanner;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Nhap canh cua 1 khoi lap phuong:");
-		int canh = scanner.nextInt();
-		System.out.println("the tich = " + Math.pow(canh, 3));// the tich = canh x canh x canh = canh ^3 =
-																// Math.pow(canh,3)
+		System.out.println("nhap diem toan:");
+		int toan = scanner.nextInt();
+		System.out.println("nhap diem van: ");
+		int van = scanner.nextInt();
+		System.out.println("nhap diem tieng anh:");
+		int tiengAnh = scanner.nextInt();
 		scanner.close();
+		int dtb = (toan + van + tiengAnh) / 3;
+		if (dtb > 5) {
+			System.out.println("Qua môn học");
+		} else if (dtb < 5) {
+			System.out.println("Rớt môn học");
+		} else {
+			System.out.println("bạn bỏ thi, rót môn học");
+		}
 	}
 }
