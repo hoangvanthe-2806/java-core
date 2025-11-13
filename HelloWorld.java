52db7d9046a125b3a98f39c27055933640df369a
hoangthe030628 <hoangthe2862003@gmail.com>
2025-11-13 15:44:48 +0700

    switch()

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,20 +5,30 @@ import java.util.Scanner;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("nhap diem toan:");
-		int toan = scanner.nextInt();
-		System.out.println("nhap diem van: ");
-		int van = scanner.nextInt();
-		System.out.println("nhap diem tieng anh:");
-		int tiengAnh = scanner.nextInt();
-		scanner.close();
-		int dtb = (toan + van + tiengAnh) / 3;
-		if (dtb > 5) {
-			System.out.println("Qua môn học");
-		} else if (dtb < 5) {
-			System.out.println("Rớt môn học");
-		} else {
-			System.out.println("bạn bỏ thi, rót môn học");
+		int day = scanner.nextInt();
+		switch (day) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2:
+			System.out.println("Tuesday");
+			break;
+		case 3:
+			System.out.println("Wednesday");
+			break;
+		case 4:
+			System.out.println("Thursday");
+			break;
+		case 5:
+			System.out.println("Friday");
+			break;
+		case 6:
+			System.out.println("Saturday");
+			break;
+		case 7:
+			System.out.println("Sunday");
+			break;
 		}
+
 	}
 }
