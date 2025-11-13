69fe4336e8a06b186b6ce2e921bbcda81a8765a9
hoangthe030628 <hoangthe2862003@gmail.com>
2025-11-13 14:29:51 +0700

    bai tap lab 03

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Chieu dai: ");
-		int cd = scanner.nextInt();
-		System.out.println("chieu rong:");
-		int cr = scanner.nextInt();
-		System.out.println("Chu vi hcn = " + ((cd + cr) * 2)); // (chieu dai+ chieu rong)*2
-		System.out.println("Dien  tich hcn = " + (cd * cr)); // chieu dai x chieu rong
-		System.out.println("Canh nho nhat: " + Math.min(cd, cr));
-
+		System.out.println("Nhap canh cua 1 khoi lap phuong:");
+		int canh = scanner.nextInt();
+		System.out.println("the tich = " + Math.pow(canh, 3));// the tich = canh x canh x canh = canh ^3 =
+																// Math.pow(canh,3)
+		scanner.close();
 	}
 }
