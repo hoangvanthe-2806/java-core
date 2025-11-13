a01970fc63c12af16cc1a42cf1feb3f38c7a000a
hoangthe030628 <hoangthe2862003@gmail.com>
2025-11-13 14:16:32 +0700

    bai tap lab 01

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,14 +5,11 @@ import java.util.Scanner;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Nhap so a:");
-		int a = scanner.nextInt();
-		System.out.println("Nhap so b:");
-		int b = scanner.nextInt();
-		int max = Math.max(a, b);
-		int min = Math.min(a, b);
-		scanner.close();
-		System.out.println("Max: " + max + ", Min: " + min);
+		System.out.println("Nhap ten sinh vien: ");
+		String name = scanner.nextLine();
+		System.out.println("DTB:");
+		int dtb = scanner.nextInt();
+		System.out.println(name + " co diem = " + dtb);
 
 	}
 }
