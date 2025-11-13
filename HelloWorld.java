2b1d7ac92334194f50c3625723b3c79843f33cfc
hoangthe030628 <hoangthe2862003@gmail.com>
2025-11-13 14:12:40 +0700

    Scanner-2 so nguyen int(Min,Max)

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class HelloWorld {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Your name is: ");
-		String name = scanner.nextLine();
-		System.out.println("Your age is: ");
-		int age = scanner.nextInt();
-		System.out.println("My name is " + name + ", age = " + age);
+		System.out.println("Nhap so a:");
+		int a = scanner.nextInt();
+		System.out.println("Nhap so b:");
+		int b = scanner.nextInt();
+		int max = Math.max(a, b);
+		int min = Math.min(a, b);
 		scanner.close();
+		System.out.println("Max: " + max + ", Min: " + min);
+
 	}
 }
