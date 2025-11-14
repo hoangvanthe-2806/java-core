9e5ce544abdff5561d68405cfb903201bd468464
hoangthe030628 <hoangthe2862003@gmail.com>
2025-11-14 16:19:55 +0700

    lab03-bt2

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,23 +4,11 @@ import java.util.Scanner;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Nhập vào 1 số nguyên bất kỳ: ");
 		Scanner scanner = new Scanner(System.in);
-		int value = scanner.nextInt();
-
-		int count = 0;
-		for (int i = 2; i < value - 1; i++) {
-			if (value % i == 0) {
-				System.out.println("Ước số khác: " + i);
-				count++;
-
-			}
-//			break;
-		}
-		if (count == 0) {
-			System.out.printf("%d là số nguyên tố", value);
-		} else {
-			System.out.printf("%d không là số nguyên tố", value);
+		System.out.println("Nhập vào 1 số nguyên bất kỳ:");
+		int x = scanner.nextInt();
+		for (int i = 1; i <= 10; i++) {
+			System.out.printf(" %d x %d = %d \n", x, i, x * i);
 		}
 		scanner.close();
 	}
